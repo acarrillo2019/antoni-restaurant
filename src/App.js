fc7671d8cc695afa98bbcbaabbede6f6e91af84a
Alfredo Carrillo <alric21@aol.com>
2022-10-24 09:13:02 -0700

    starter

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
-
 import './App.css';
 
 const App = () => (
-    <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Antoni's Fine Italian
-        </p>
-      </header>
-      
-    </div>
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
 );
-
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,75 +1,11 @@
 import React from 'react';
 
-
 import './Navbar.css';
 
-function Navbar() {
-  return (
-    <nav className='app__navbar'>
-      <div class="container">
-        <a class="logo-container" href="/">
-          <img src="" class="logo" alt="logo">
-        </a>
-        <div class="nav-items large">
-          <div class="item-container item-lg">
-            <li class="item">WHO WE ARE?</li>
-          </div>
-          <div class="item-container">
-            <li class="item"><span class="txt">Location</span>
-              <img src="" alt="down" class="down">
-            </li>
-            <ul class="item-dropdown"><li class="item-drop-ele">Blip</li>
-              <li class="item-drop-ele">Bloop</li>
-            </ul>
-          </div>
-          <div class="item-container">
-            <li class="item"><span class="txt">SHOP</span>
-              <img src="" alt="down" class="down">
-            </li>
-            <ul class="item-dropdown">
-              <a class="item-drop-ele" href="/shop/filter">ghy</a>
-              <a class="item-drop-ele" href="/shop/espresso">ESPRESSO</a>
-              <a class="item-drop-ele" href="/shop/merchandise">MERCHANDISE</a>
-            </ul>
-          </div>
-          <div class="item-container item-lg">
-            <li class="item">CATERING</li>
-          </div>
-          <div class="item-container">
-            <li class="item">CONTACT</li>
-          </div>
-          <div class="item-container">
-            <li class="item">
-              <img src="" alt="cart" class="cart">
-                <span class="txt">CART</span>
-            </li>
-          </div>
-          <div class="item-container item-md">
-            <li class="item"><img src="" alt="user" class="user">
-              <span class="txt">ACCOUNT</span>
-            </li>
-          </div>
-        </div>
-        <div class="nav-items small">
-          <div class="item-container">
-            <li class="item">
-              <img src="" alt="shop-bag" class="icon">
-            </li>
-          </div>
-          <div class="item-container">
-            <li class="item">
-              <img src="" alt="cart" class="icon">
-            </li>
-          </div>
-          <div class="item-container">
-            <li class="item">
-              <img src="" alt="menu" class="icon">
-            </li>
-          </div>
-        </div>
-      </div>
-    </nav>
-  )
-}
+const Navbar = () => (
+  <div>
+    Navbar
+  </div>
+);
 
-export default Navbar
+export default Navbar;
